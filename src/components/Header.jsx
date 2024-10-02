6188442f3423ebb11ddac4c39c57f865fb0d6c0d
kwonboryong <kwonboryong@naver.com>
2024-10-02 16:01:09 +0900

    Update: 시안 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,38 @@
 import s from "./Header.module.scss";
+import { motion } from "framer-motion";
 
 function Header() {
   return (
     <div className={s.container}>
       <div className={s.leftbox}>
-        <a
+        <motion.a
           href="https://github.com/kwonboryong"
           target="_blank"
           rel="noopener noreferrer"
+          whileHover={{
+            scale: 1.2,
+            background: "radial-gradient(circle, #F3F781 0%, #FFFFFF 100%)",
+          }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ duration: 0.2 }}
+          // transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
           Github
-        </a>
-        <a
-          href="https://tensdiary.tistory.com/"
-          target="_blank"
-          rel="noopener noreferrer"
+        </motion.a>
+        <motion.a
+          className="box"
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          Tistory
-        </a>
+          <a
+            href="https://tensdiary.tistory.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tistory
+          </a>
+        </motion.a>
       </div>
 
       <div className={s.rightbox}>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,7 @@ function Profile() {
     <div className={s.container}>
       <img
         className={s.profile}
-        src="/assets/profile.webp"
+        src="/assets/profileRemove.webp"
         alt="개발자 권보령 프로필 사진"
       />
     </div>
