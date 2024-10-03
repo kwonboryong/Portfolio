88dfef5629c5012daea6027d770c1f99048fc441
kwonboryong <kwonboryong@naver.com>
2024-10-04 00:00:04 +0900

    Update: 스크롤 페이지 이동 애니메이션

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,34 @@
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Main from "@/pages/Main";
 import IntroducePage from "@/pages/IntroducePage";
 import SkillsPage from "@/pages/SkillsPage";
 import ProjectsPage from "@/pages/ProjectsPage";
 
 function HomePage() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+
   return (
     <div>
-      <Main />
-      <IntroducePage />
-      <SkillsPage />
-      <ProjectsPage />
+      <motion.div
+        style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: -100 }}
+      >
+        <Main />
+      </motion.div>
+
+      <div ref={ref} style={{ marginTop: "100vh" }}>
+        <motion.div
+          style={{
+            y: useTransform(scrollYProgress, [0, 1], [0, 0]),
+            // transition: "y 0.5s ease-out",
+          }}
+        >
+          <IntroducePage />
+          <SkillsPage />
+          <ProjectsPage />
+        </motion.div>
+      </div>
     </div>
   );
 }
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,20 +3,20 @@ import { motion } from "framer-motion";
 
 function ProjectsPage() {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: false }}
-      transition={{
-        ease: "easeInOut",
-        duration: 2,
-        y: { duration: 1 },
-      }}
-    >
+    // <motion.div
+    //   initial={{ opacity: 0, y: 50 }}
+    //   whileInView={{ opacity: 1, y: 0 }}
+    //   viewport={{ once: false }}
+    //   transition={{
+    //     ease: "easeInOut",
+    //     duration: 2,
+    //     y: { duration: 1 },
+    //   }}
+    // >
       <div>
         <div className={s.container}>dkdkdk</div>
       </div>
-    </motion.div>
+    // </motion.div>
   );
 }
 
